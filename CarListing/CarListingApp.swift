2418b021d9c46f6c51b55b9a6a703748263f7dae
M. A. Alim Mukul <mukul.cse2010@gmail.com>
2022-09-27 15:17:40 +0200

    Initial Commit

---
+++ CarListing/CarListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarListingApp.swift
+//  CarListing
+//
+//  Created by M. A. Alim Mukul on 27.09.22.
+//
+
+import SwiftUI
+
+@main
+struct CarListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarListing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarListing
+//
+//  Created by M. A. Alim Mukul on 27.09.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
